a10062e1f3705fccc01980d921d2c74f586f6a98
Joe Klause <joeklause@gmail.com>
2019-07-12 12:53:55 -0700

    zero message

--- app.js
+++ app.js
@@ -3,22 +3,36 @@ import isEven from './is-even.js';
 // referenced elements from index.html
 const userInput = document.getElementById('user-input');
 const button = document.getElementById('button');
+
 const evenImage = document.getElementById('even-image');
 const oddImage = document.getElementById('odd-image');
-const evenMsg = document.getElementById('even-message');
-const oddMsg = document.getElementById('odd-message')
 
+const evenMsg = document.getElementById('even-message');
+const oddMsg = document.getElementById('odd-message');
+const zeroMsg = document.getElementById('zero-message');
+
+// wire it up!
 button.onclick = function() {
-    const result = isEven(Number(userInput.value));
-    if(result === true) {
-        evenImage.classList.remove('hidden');
-        evenMsg.classList.remove('hidden');
-        oddImage.classList.add('hidden');
-        oddMsg.classList.add('hidden');
-    } else {
+    if (Number(userInput.value) === 0) {
+        zeroMsg.classList.remove('hidden');
         evenImage.classList.add('hidden');
         evenMsg.classList.add('hidden');
-        oddImage.classList.remove('hidden');
-        oddMsg.classList.remove('hidden');
-    }
+        oddImage.classList.add('hidden');
+        oddMsg.classList.add('hidden');
+    } else { 
+        const result = isEven(Number(userInput.value));
+        if(result === true) {
+            evenImage.classList.remove('hidden');
+            evenMsg.classList.remove('hidden');
+            oddImage.classList.add('hidden');
+            oddMsg.classList.add('hidden');
+            zeroMsg.classList.add('hidden');
+        } else {
+            evenImage.classList.add('hidden');
+            evenMsg.classList.add('hidden');
+            oddImage.classList.remove('hidden');
+            oddMsg.classList.remove('hidden');
+            zeroMsg.classList.add('hidden');
+        }
+    }; 
 };
