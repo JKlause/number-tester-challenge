87cf7ea12155c992534000f9526f1d058b795345
Joe Klause <joeklause@gmail.com>
2019-07-12 17:46:05 -0700

    fully functioning with zero and noInput

--- app.js
+++ app.js
@@ -10,16 +10,28 @@ const oddImage = document.getElementById('odd-image');
 const evenMsg = document.getElementById('even-message');
 const oddMsg = document.getElementById('odd-message');
 const zeroMsg = document.getElementById('zero-message');
+const noInputMsg = document.getElementById('no-input-message');
 
 // wire it up!
 button.onclick = function() {
-    if (Number(userInput.value) === 0) {
-        zeroMsg.classList.remove('hidden');
+         // If user doesn't enter anything...
+    if(userInput.value === '') {
         evenImage.classList.add('hidden');
         evenMsg.classList.add('hidden');
         oddImage.classList.add('hidden');
         oddMsg.classList.add('hidden');
-    } else { 
+        zeroMsg.classList.add('hidden');
+        noInputMsg.classList.remove('hidden');
+    }    //if user enters 0 
+      else if (Number(userInput.value) === 0) {
+        evenImage.classList.add('hidden');
+        evenMsg.classList.add('hidden');
+        oddImage.classList.add('hidden');
+        oddMsg.classList.add('hidden');
+        zeroMsg.classList.remove('hidden');
+        noInputMsg.classList.add('hidden');
+    }    //if user enters a number
+      else {  
         const result = isEven(Number(userInput.value));
         if(result === true) {
             evenImage.classList.remove('hidden');
@@ -27,12 +39,14 @@ button.onclick = function() {
             oddImage.classList.add('hidden');
             oddMsg.classList.add('hidden');
             zeroMsg.classList.add('hidden');
+            noInputMsg.classList.add('hidden');
         } else {
             evenImage.classList.add('hidden');
             evenMsg.classList.add('hidden');
             oddImage.classList.remove('hidden');
             oddMsg.classList.remove('hidden');
             zeroMsg.classList.add('hidden');
+            noInputMsg.classList.add('hidden');
         }
-    }; 
-};
+    };
+}; 
